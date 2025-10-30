024894c7ae6fe1cb7253479a7c06f22d01774c24
Abdellah <hammouchabdellah4529@gmail.com>
2025-10-30 12:51:41 +0100

    remplissage par des nombres la dernière div

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+let jours = document.querySelectorAll(".case");
+let formContainer = document.querySelector(".formContainer");
+let nom = document.querySelector("#clientName");
+let fermer = document.querySelector(".fermer");
+let form ;
+
+
+for (let i = 0; i < jours.length; i++) {
+    jours[i].textContent = i+1;
+}
+
 
