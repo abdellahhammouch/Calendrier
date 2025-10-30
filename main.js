63286da8be3824720541ca572e9cc0c63c20cf82
Abdellah <hammouchabdellah4529@gmail.com>
2025-10-30 19:10:45 +0100

    avancement dans la validation des étiquettes

--- main.js
+++ main.js
@@ -1,25 +1,39 @@
-let jours = document.querySelectorAll(".jour");
+let jour = document.querySelectorAll(".jour");
 let formContainer = document.querySelector(".formContainer");
-let nom = document.querySelector("#clientName");
+let containerJour = document.querySelector(".calendrierG");
+id = 0;
+let données = {
+    id : document.querySelector(".reservationId").value,
+    nom: document.querySelector("#clientName").value,
+    date: document.querySelector("#tempsReservation").value,
+    nbrP: document.querySelector("#nombreDePersonnes").value,
+    typeR: document.querySelector("#reservationType").value,
+};
 let fermer = document.querySelector(".fermer");
-let form ;
+let submit = document.querySelector("#reserverButton");
+let form;
 
-
-
-
-for (let j = 0; j < 5; j++) {
-    jours[j].addEventListener('click', () => {
-        formContainer.style.display = "flex";
-        form = true;
-    })
+for (let j = 0; j < 7; j++) {
+  jour[j].addEventListener("click", () => {
+    formContainer.style.display = "flex";
+    form = true;
+  });
 }
 
-fermer.addEventListener('click', () => {
-        formContainer.style.display = "none";
-        form = false;
-})
-
+fermer.addEventListener("click", () => {
+  formContainer.style.display = "none";
+  form = false;
+});
 
 if (form === true) {
-    
+  submit.addEventListener("submit", () => {
+    id ++;
+    let ticket = document.createElement("div");
+
+    let infoTicket = document.createElement("p");
+    infoTicket.textContent = `${données.nom} ${données.date} ${données.nbrP} ${données.typeR}`;
+    ticket.appendChild(infoTicket);
+
+
+  });
 }
