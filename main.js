a02780b38efb496b80db8912662c9122e10f23f2
Abdellah <hammouchabdellah4529@gmail.com>
2025-10-30 14:52:44 +0100

    ajout des div contenant les jours

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-let jours = document.querySelectorAll(".case");
+let jours = document.querySelectorAll(".jour");
 let formContainer = document.querySelector(".formContainer");
 let nom = document.querySelector("#clientName");
 let fermer = document.querySelector(".fermer");
 let form ;
 
 
-for (let i = 0; i < jours.length; i++) {
-    jours[i].textContent = i+1;
-    jours[i].style.
-}
+
 
 for (let j = 0; j < 5; j++) {
     jours[j].addEventListener('click', () => {
