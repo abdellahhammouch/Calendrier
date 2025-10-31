8f8e990f8f2016cf63405b2f017a5f490eef50b7
Abdellah <hammouchabdellah4529@gmail.com>
2025-10-31 10:28:40 +0100

    selection des élements en JS

---
+++ main.js
@@ -0,0 +1,5 @@
+const jours = document.querySelectorAll(".jour");
+const formContainer = document.querySelector(".formContiner");
+const formulaire = document.querySelector("#formulaire");
+const fermer = document.querySelector(".fermer");
+
