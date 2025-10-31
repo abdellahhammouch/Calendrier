161f5825606002335847adbdf47db9d15029dcfb
Abdellah <hammouchabdellah4529@gmail.com>
2025-10-31 11:39:22 +0100

    ajout de la confirmation de la formulaire

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 const jours = document.querySelectorAll(".jour");
-const formContainer = document.querySelector(".formContiner");
+const formContainer = document.querySelector(".formContainer");
 const formulaire = document.querySelector("#formulaire");
 const fermer = document.querySelector(".fermer");
+
+let jourActif = null;
 
 jours.forEach((jour, indice) => {
   if (indice < 5) {
     jour.addEventListener("click",() => {
-      formContainer.computedStyleMap.display = "flex";
+      formContainer.style.display = "flex";
+      jourActif = jour;
     });
   }
 });
 
-fermer.addEventListener("click", () => {
+fermer.addEventListener("click", ()=> {
   formContainer.style.display = "none";
+  jourActif = null;
+})
+
+formContainer.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const infoUser = {
+    id : Date.now(),
+    clientName : document.querySelector("#clientName").value.trim(),
+    tempsR : document.querySelector("#tempsReservation").value,
+    nbrP : document.querySelector("#nombreDePersonnes").value,
+    typeR : document.querySelector("#reservationType").value
+  }
 })
