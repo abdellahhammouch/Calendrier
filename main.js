533bf09fc039b3d65284a66791c6aafef90c93c2
Abdellah <hammouchabdellah4529@gmail.com>
2025-10-31 16:20:06 +0100

    modification dans la fonction de suprression

--- main.js
+++ main.js
@@ -6,71 +6,76 @@ const fermer = document.querySelector(".fermer");
 let jourActif = null;
 
 jours.forEach((jour, indice) => {
-    jour.addEventListener("dblclick",() => {
-      formContainer.style.display = "flex";
-      jourActif = jour;
-    });
+  jour.addEventListener("dblclick", () => {
+    formContainer.style.display = "flex";
+    jourActif = jour;
+  });
 });
 
-fermer.addEventListener("click", ()=> {
+fermer.addEventListener("click", () => {
   formContainer.style.display = "none";
   jourActif = null;
-})
+});
 
 formContainer.addEventListener("submit", (e) => {
   e.preventDefault();
   const infoUser = {
-    id : Date.now(),
-    clientName : document.querySelector("#clientName").value.trim(),
-    tempsR : document.querySelector("#tempsReservation").value,
-    nbrP : document.querySelector("#nombreDePersonnes").value,
-    typeR : document.querySelector("#reservationType").value
-  }
-
+    id: Date.now(),
+    clientName: document.querySelector("#clientName").value.trim(),
+    tempsR: document.querySelector("#tempsReservation").value,
+    nbrP: document.querySelector("#nombreDePersonnes").value,
+    typeR: document.querySelector("#reservationType").value,
+  };
 
   const ticket = document.createElement("div");
+  ticket.style.display = "flex";
   ticket.classList.add("reservation", infoUser.typeR.toLowerCase());
   ticket.textContent = `${infoUser.clientName} - ${infoUser.tempsR} - ${infoUser.nbrP} pers. - ${infoUser.typeR}`;
-  ticket.style.zIndex = "1";
+  const boutonSupprimer = document.createElement("button");
+  boutonSupprimer.textContent = "✖";
+  boutonSupprimer.classList.add("btn-supprimer");
+  ticket.appendChild(boutonSupprimer);
+  boutonSupprimer.style.justifyContent = "flex-end";
+  boutonSupprimer.style.cursor = "pointer";
+  boutonSupprimer.style.border = "none";
+  boutonSupprimer.style.background = "transparent";
+  boutonSupprimer.style.fontWeight = "bold";
 
   jourActif.appendChild(ticket);
+  ticket.appendChild(boutonSupprimer);
   formContainer.style.display = "none";
 
   let color;
   switch (infoUser.typeR) {
-    case 'vip':
+    case "vip":
       color = "#E62727";
       break;
-    case 'standard':
+    case "standard":
       color = "#1E93AB";
       break;
-    case 'anniversaire':
+    case "anniversaire":
       color = "#07c507ff";
       break;
-    case 'groupe':
+    case "groupe":
       color = "#DCDCDC";
       break;
-  
+
     default:
       break;
   }
 
-  ticket.style.backgroundColor = color ;
+  ticket.style.backgroundColor = color;
   ticket.style.borderRadius = "10px";
   ticket.style.padding = "1%";
-  ticket.style.marginBottom = "2%"
+  ticket.style.marginBottom = "2%";
 
   formulaire.reset();
 
-  ticket.addEventListener("click", () => {
-  const confirmation = confirm("Voulez-vous vraiment supprimer cette réservation ?");
-  if (confirmation) {
-    ticket.remove();
-  }
-  })
-})
-
-
-
-
-
+  boutonSupprimer.addEventListener("click", () => {
+    const confirmation = confirm("Voulez-vous vraiment supprimer cette réservation ?"
+    );
+    if (confirmation) {
+      ticket.remove();
+    }
+  });
+});
